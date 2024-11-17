8b58e21d045c30b74076ee2e546fc281a5faaf65
abdulwahababdal <abdulwahababdal@gmail.com>
2024-11-17 15:10:16 +0300

    com

--- api/posts/posts.controllers.js
+++ api/posts/posts.controllers.js
@@ -1,18 +1,9 @@
-const Post = require('../../models/Post');
+const Post = require("../../models/Post");
 
 exports.fetchPost = async (postId, next) => {
   try {
     const post = await Post.findById(postId);
     return post;
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.postsCreate = async (req, res) => {
-  try {
-    const newPost = await Post.create(req.body);
-    res.status(201).json(newPost);
   } catch (error) {
     next(error);
   }
@@ -38,7 +29,10 @@ exports.postsUpdate = async (req, res) => {
 
 exports.postsGet = async (req, res) => {
   try {
-    const posts = await Post.find();
+    const posts = await Post.find({}, "-createdAt -updatedAt").populate(
+      "authorId",
+      "name"
+    );
     res.json(posts);
   } catch (error) {
     next(error);
